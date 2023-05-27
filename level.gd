37d2ea6a854683adc1790819e0d3cdc706a00316
Ted Steen <ted.steen@gmail.com>
2023-05-28 00:11:15 +0300

    prepare for more entity types on the map

--- level.gd
+++ level.gd
@@ -1,6 +1,7 @@
 extends Node2D
 var cursor_res = preload("res://cursor.tscn")
 var pot_res = preload("res://entities/pot.tscn")
+var door_res = preload("res://entities/door.tscn")
 var rng: RandomNumberGenerator
 
 @onready var background_rect = $Container/BackgroundRect
@@ -9,42 +10,51 @@ var rng: RandomNumberGenerator
 @onready var dss: = get_world_2d().direct_space_state
 
 @export var cursor_lifetime = 5
-@export var seed = 0
+@export var map_seed = 1
 
 var time
 var frame
 var current_recording: Cursor
 var cursors: Array[Cursor]
-var pots: Array[Pot]
+var entities: Array[Entity]
+
 var point_params: = PhysicsPointQueryParameters2D.new()
 
 const CELL_SIZE = 90
 
 func _ready():
 	rng = RandomNumberGenerator.new()
-	rng.seed = seed
+	rng.seed = map_seed
 	point_params.collide_with_areas = true #set it up
 	#point_params.collision_mask = 2
 	cursors = []
 	reset_time(true)
 
-func generate_pot_data(difficulty: int, amount: int) -> Array[Dictionary]:
-	var available_data: Array[Dictionary] = []
-	for x in range(0, background_rect.size.x / CELL_SIZE):
-		for y in range(0, background_rect.size.y / CELL_SIZE):
-			var data = { "health": rng.randi() % difficulty + 1, "position": Vector2(x * CELL_SIZE, y * CELL_SIZE) }
-			available_data.append(data)
-
-	var pot_data: Array[Dictionary] = []
-	if amount > available_data.size():
-		push_warning("Can't generate %d pots, there are only %d available." % [amount, available_data.size()])
-		amount = available_data.size()
+func generate_map_data(difficulty: int, amount: int) -> Array[Dictionary]:
+	var width = int(background_rect.size.x / CELL_SIZE)
+	var height = int(background_rect.size.y / CELL_SIZE)
+	
+	var available_stuff: Array[Dictionary] = []
+	var free_slots: Array = range(0, width*height)
+	
 	for i in range(0, amount):
-		var x = rng.randi() % available_data.size()
-		pot_data.append(available_data[x])
-		available_data.remove_at(x)
-		
-	return pot_data
+		available_stuff.append({"type": "pot", "health": rng.randi() % difficulty + 1 })
+	
+	available_stuff.append({ "type": "door" })
+	
+	if available_stuff.size() > width * height:
+		push_error("Can't fit content on map.")
+		return []
+	
+	var map_data: Array[Dictionary] = []
+	for stuff in available_stuff:
+		var idx = rng.randi() % free_slots.size()
+		var coord = free_slots[idx]
+		stuff["position"] = Vector2((coord % width) * CELL_SIZE, int(coord / width) * CELL_SIZE)
+		map_data.append(stuff)
+		free_slots.remove_at(idx)
+
+	return map_data
 
 func reset_time(new_pots: bool):
 	time = 0
@@ -56,15 +66,23 @@ func reset_time(new_pots: bool):
 	add_child(current_recording)
 
 	if new_pots:
-		for pot in pots:
-			remove_child(pot)
-		for pot_data in generate_pot_data(100, 40):
-			var pot: Pot = pot_res.instantiate()
-			pot.position = pot_data.position
-			pots.append(pot)
-			add_child(pot)
-			pot.health = pot_data.health
-
+		for entity in entities:
+			if entity is Pot:
+				remove_child(entity)
+		for map_data in generate_map_data(100, 10):
+			var position = map_data.position
+			if map_data.type == "pot":
+				var pot: Pot = pot_res.instantiate()
+				pot.position = position
+				entities.append(pot)
+				add_child(pot)
+				pot.health = map_data.health
+			elif map_data.type == "door":
+				var door: Door = door_res.instantiate()
+				door.position = position
+				entities.append(door)
+				add_child(door)
+				
 func _physics_process(delta):	
 	time += delta
 	if time >= cursor_lifetime:
@@ -87,6 +105,8 @@ func handle_click(event: InputEventMouse):
 			var entity = hit.collider
 			if entity is Pot:
 				entity.take_damage()
+			elif entity is Door:
+				print_debug("Knock...")
 
 func _input(event):
 	if event is InputEventMouse:
